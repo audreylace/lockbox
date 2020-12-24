39e5153e942b6da3149861f88e636b8f79afa321
Audrey Lace <audrey.lace.taylor@gmail.com>
2020-12-23 22:25:12 -0600

    Add debug messages

--- app/scripts/comp/aspcom/aspcom-setting-store.ts
+++ app/scripts/comp/aspcom/aspcom-setting-store.ts
@@ -1,8 +1,9 @@
 import { ASPComServerModel } from './aspcom-server-model';
 import { Logger } from 'util/logger';
 import { SettingsStoreLocal } from 'comp/settings/settings-store-local';
+import { ASPComEnabledState } from './index';
 
-const logger = new Logger('webserversettingssync');
+const logger = new Logger('ASPComSettingsStore');
 
 export class ASPComSettingsStore {
     static supportedKeys: string[] = ['app-settings'];
@@ -17,26 +18,26 @@ export class ASPComSettingsStore {
     static async load<T = any>(key: string): Promise<T> {
         if (this.supportedKeys.indexOf(key) !== -1) {
             if (this.cache.has(key)) {
+                logger.debug(`returned key ${key}`);
                 return this.cache.get(key);
             } else {
                 return undefined;
             }
         } else {
+            logger.debug(`key ${key} not supported, loading from browser instead`);
             return SettingsStoreLocal.load(key);
         }
-        /* try {
-            const results = await ASPComServerModel.loadSetting(key);
-            this.cache.set(key, results.value);
-            return results.value;
-        } catch (error) {
-            logger.error(`Error loading ${key} from remote webserver`, error);
-            throw error;
-        } */
     }
 
     static async save<TValue>(key: string, value: TValue): Promise<void> {
         if (this.supportedKeys.indexOf(key) !== -1) {
+            if (ASPComServerModel.inTestingMode) {
+                logger.info('Skipping save to remote server since in testing mode');
+                return;
+            }
+
             try {
+                logger.debug('Attempting to sync settings to remote server');
                 await ASPComServerModel.saveSetting(key, value);
                 this.cache.set(key, value);
             } catch (error) {
@@ -44,6 +45,7 @@ export class ASPComSettingsStore {
                 throw error;
             }
         } else {
+            logger.debug('Key not supported, saving to browser instead');
             return SettingsStoreLocal.save(key, value);
         }
     }
